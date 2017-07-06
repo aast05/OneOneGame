111b37d7663e8b751978b8a94f85fad6cac7cd11
aenplus <aenplus01@gmail.com>
2017-07-06 23:48:07 +0200

    add : displayer class

---
+++ src/main/java/com/aenplus/oog/graphics/GameDisplayer.java
@@ -0,0 +1,8 @@
+package com.aenplus.oog.graphics;
+
+import com.aenplus.oog.game.Game;
+
+public interface GameDisplayer {
+
+	public void display(Game game);
+}
---
+++ src/main/java/com/aenplus/oog/graphics/impl/Displayer.java
@@ -0,0 +1,37 @@
+package com.aenplus.oog.graphics.impl;
+
+import java.util.List;
+
+import com.aenplus.oog.entity.Player;
+import com.aenplus.oog.game.Game;
+import com.aenplus.oog.graphics.GameDisplayer;
+import com.aenplus.oog.table.Cell;
+
+public class Displayer implements GameDisplayer {
+
+	public void display(Game game) {
+		String res = "| ";
+		String lineFormat = "%s | ";
+		int height = game.getTable().getNbRows();
+		int width = game.getTable().getNbColumn();
+		List<Cell> cells = game.getTable().getCells();
+		int i = 0;
+		for (Cell cell : cells) {
+			if (i == width) {
+				res += "\n| " + lineFormat.format(lineFormat, cellContent(cell.getPlayer()));
+				i = 0;
+			} else
+				res += lineFormat.format(lineFormat, cellContent(cell.getPlayer()));
+			i++;
+		}
+		System.out.println(res);
+	}
+
+	private String cellContent(Player player) {
+		if (player != null) {
+			return player.getName();
+		}
+		return "_";
+	}
+
+}
